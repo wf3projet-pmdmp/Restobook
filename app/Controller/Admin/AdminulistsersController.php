95dbfc31d49399e45b25fdf54523f707f4e1c057
Pascal PROSPA <contact@pascalprospa.com>
2017-04-13 10:34:34 -0400

    13/04/ 10H30

---
+++ app/Controller/Admin/AdminulistsersController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Controller\Admin;
+
+use \W\Controller\Controller;
+use Model\AdminModel;
+use Model\GestrestoModel;
+use Model\RestaurantsModel;
+use Model\UsersModel;
+
+class AdminusersController extends Controller
+{
+
+    // Dossiers contenant les vues
+    const PATH_VIEWS = 'admin/utilisateurs';
+
+    public function listAll()
+    {
+
+        $usersModel = new UsersModel();
+        $users = $usersModel->findAll();
+        $params = [
+            'users' => $users
+        ];
+        $this->show(SELF::PATH_VIEWS.'/list', $params);
+    }
+
+    public function add()
+    {
+        $this->show(SELF::PATH_VIEWS.'/add');
+
+    }
+
+    public function view($id)
+    {
+         $usersModel = new UsersModel();
+        $users = $usersModel->find($id);
+        $params = [
+            'users' => $users,
+        ];
+        $this->show(SELF::PATH_VIEWS.'/view', $params);
+
+    }
+
+    public function up($id)
+    {
+
+        $this->show(SELF::PATH_VIEWS.'/up');
+    }
+
+    public function del($id)
+    {
+
+        $this->show(SELF::PATH_VIEWS.'/del');
+    }
+}
--- app/Views/users/logout.php
+++ app/Views/users/logout.php
@@ -1,7 +1,16 @@
 <?php $this->layout('layout', ['title' => 'Accueil']) ?>
 
 <?php $this->start('main_content') ?>
-<h2>Let's code.</h2>
-<p>Vous avez atteint la page d'accueil. Bravo.</p>
-<p>Et maintenant, RTFM dans <strong><a href="../docs/tuto/" title="Documentation de W">docs/tuto</a></strong>.</p>
+<!-- Small modal pris sur http://www.bootply.com/Ztz1JW1Kb9-->
+<button class="btn btn-primary" data-toggle="modal" data-target=".bs-example-modal-sm">Se déconnecter</button>
+
+<div class="modal bs-example-modal-sm" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-sm">
+    <div class="modal-content">
+      <div class="modal-header"><h4>Déconnexion <i class="fa fa-lock"></i></h4></div>
+      <div class="modal-body"><i class="fa fa-question-circle"></i> Etes vous sûr de vous déconnecter ?</div>
+      <div class="modal-footer"><a href="javascript:;" class="btn btn-primary btn-block">Appuyez içi pour vous déconnecter</a></div>
+    </div>
+  </div>
+</div>
 <?php $this->stop('main_content') ?>
